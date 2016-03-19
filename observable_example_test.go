ed77cf14ca729ba734ca94eff2b24cf21f34a50a
Stefan Richter <stefan.richter@freiheit.com>
2016-03-19 18:05:29 +0100

    add example/README for all operators

--- observable_example_test.go
+++ observable_example_test.go
@@ -1,7 +1,3 @@
-# A very thin functional reactive programming layer over Go's CSP primitives
-
-
-```go
 package daisychain
 
 import "fmt"
@@ -16,19 +12,19 @@ func Example() {
 	o2 := Create(
 		// o1 emits 0..9!
 		o1,
-		// map adds 1 to each number
+		// add 1 to each number
 		Map(func(ev Event) Event {
 			return ev.(int) + 1
 		}),
-		// skips 1..5
+		// skip 1..5
 		Skip(5),
-		// takes 6,7,8
+		// take 6,7,8
 		Take(3),
-		// filters even numbers: 6,8
+		// filter even numbers: 6,8
 		Filter(func(ev Event) bool {
 			return ev.(int)%2 == 0
 		}),
-		// sums 6 and 8
+		// sum of 6 and 8
 		Reduce(func(ev1, ev2 Event) Event {
 			return ev1.(int) + ev2.(int)
 		}, 0),
@@ -44,5 +40,3 @@ func Example() {
 func onComplete(ev Event) {
 	fmt.Println(ev)
 }
-
-```
